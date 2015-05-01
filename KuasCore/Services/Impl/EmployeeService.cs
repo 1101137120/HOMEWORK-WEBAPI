26bdabbb992d54ed2ab2a85e4e9c00f147631510
1101137120 <mike255920002000@gmail.com>
2015-05-01 17:24:26 +0800

    WebApi

---
+++ KuasCore/Services/Impl/EmployeeService.cs
@@ -0,0 +1,49 @@
+﻿using KuasCore.Dao;
+using KuasCore.Dao.Impl;
+using KuasCore.Models;
+using System.Collections.Generic;
+
+namespace KuasCore.Services.Impl
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        public IEmployeeDao EmployeeDao { get; set; }
+
+        public void AddEmployee(Employee employee)
+        {
+            EmployeeDao.AddEmployee(employee);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            EmployeeDao.UpdateEmployee(employee);
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            employee = EmployeeDao.GetEmployeeByName(employee.Name);
+
+            if (employee != null)
+            {
+                EmployeeDao.DeleteEmployee(employee);
+            }
+        }
+
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeDao.GetAllEmployees();
+        }
+        public Employee GetEmployeeById(string Id)
+        {
+            return EmployeeDao.GetEmployeeById(Id);
+        }
+
+        public Employee GetEmployeeByName(string Name)
+        {
+            return EmployeeDao.GetEmployeeByName(Name);
+        }
+
+    }
+
+}
